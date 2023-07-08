c1b06ed91a78b078565eda21d182e1437585ebd5
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 05:15:24 +0300

    utils.mk: add per-target messages

--- utils.mk
+++ utils.mk
@@ -1,12 +1,15 @@
 include config.mk
 
 depscheck:
+	@echo "=======> Check if any dependencies are missing"
 	@which ${CRYPTSETUP_BIN} blkid mkswap
 
 podchecker:
+	@echo "=======> Check PODs for syntax errors"
 	@podchecker *.pod
 
 shellcheck:
+	@echo "=======> Check shell scripts for syntax errors"
 	@shellcheck -s sh cryptmount.in
 
 .PHONY: depscheck podchecker shellcheck
