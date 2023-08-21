6be70e287109b916b07bfbea4ff258f42b70a524
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-21 21:28:10 +0300

    config.mk: remove cryptsetup-bin var

--- config.mk
+++ config.mk
@@ -3,9 +3,6 @@ NAME    = cryptmount
 VERSION = 0.2
 DIST    = ${NAME}-${VERSION}
 
-# cryptsetup(8) default binary
-CRYPTSETUP_BIN = cryptsetup
-
 # paths
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
