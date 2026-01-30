394fbad95cabbd5d9d25fc4a85eed5466a589a19
Alexandr Savca <alexandr.savca89@gmail.com>
2026-01-30 21:46:24 +0200

    config.mk: add BASHCOMPDIR and VIMFILESDIR

--- config.mk
+++ config.mk
@@ -5,3 +5,5 @@ VERSION     = 0.4
 # paths
 PREFIX      = /usr/local
 MANPREFIX   = $(PREFIX)/share/man
+BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
+VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
