8ab4cfef9d1ccb01122303d2e6bd4312a8016074
Alexandr Savca <alexandr.savca89@gmail.com>
2026-01-30 21:44:32 +0200

    config.mk: minor cosmetic change

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # project metadata
-NAME      = cryptmount
-VERSION   = 0.4
+NAME        = cryptmount
+VERSION     = 0.4
 
 # paths
-PREFIX    = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+PREFIX      = /usr/local
+MANPREFIX   = $(PREFIX)/share/man
